1913cc6adc8cafa0c1db724fb9d4e19d2be9ff34
ebifrier <ebifrier@gmail.com>
2015-09-11 17:38:48 +0900

    ニコ動のスナップショットAPIのテストを追加しました。

--- Ragnarok/NicoNico/Video/Tests/VideoDataTest.cs
+++ Ragnarok/NicoNico/Video/Tests/VideoDataTest.cs
@@ -13,6 +13,13 @@ namespace Ragnarok.NicoNico.Video.Tests
     [TestFixture()]
     internal sealed class VideoDataTest
     {
+        enum RemoveTagType
+        {
+            None,
+            All,
+            ATagOnly,
+        }
+
         private CookieContainer cc;
 
         [SetUp()]
@@ -26,18 +33,25 @@ namespace Ragnarok.NicoNico.Video.Tests
         /// <summary>
         /// htmlのタグを削除します。
         /// </summary>
-        private string RemoveTag(bool removeTag, string html)
+        private string RemoveTag(RemoveTagType removeTagType, string html)
         {
-            if (!removeTag)
+            switch (removeTagType)
             {
-                return html;
+                case RemoveTagType.None:
+                    return html;
+                    
+                case RemoveTagType.All:
+                    var result = Regex.Replace(html, @"<\w+[^>]*?>", "");
+                    return Regex.Replace(result, @"</?\w+( /)?>", "");
+
+                case RemoveTagType.ATagOnly:
+                    return Regex.Replace(html, @"<a[^>]*?>((sm|mylist|user).*?)</a>", "$1");
             }
 
-            var result = Regex.Replace(html, @"<\w+[^>]*?>", "");
-            return Regex.Replace(result, @"</?\w+( /)?>", "");
+            return string.Empty;
         }
 
-        private void TestSM9(VideoData video, bool removeTag)
+        private void TestSM9(VideoData video, RemoveTagType removeTagType)
         {
             Assert.IsNotNull(video);
 
@@ -55,15 +69,15 @@ namespace Ragnarok.NicoNico.Video.Tests
                 "陰陽師", "レッツゴー！陰陽師", "公式", "音楽", "ゲーム"
             };
             Assert.LessOrEqual(tags.Count(), video.TagList.Count());
-            tags.ForEachWithIndex((_, i) => Assert.AreEqual(_, video.TagList[i]));
+            tags.ForEach(_ => Assert.True(video.TagList.Contains(_)));
         }
 
-        private void TestSM941537(VideoData video, bool removeTag)
+        private void TestSM941537(VideoData video, RemoveTagType removeTagType)
         {
             Assert.AreEqual("sm941537", video.IdString);
             Assert.AreEqual(-1, video.ThreadId);
             Assert.AreEqual("ボーカロイド　初音ミク　デモソング", video.Title);
-            Assert.AreEqual(RemoveTag(removeTag,
+            Assert.AreEqual(RemoveTag(removeTagType,
                 "クリプトン開発のVOCALOID。つまり音声合成ソフトです　　　" +
                 "　　　　　　　　　　　　　　　　　　　　　　　　　　　　" +
                 "　　　　　　　　　CV：藤田咲（主な出演作＊ TVアニメ「と" +
@@ -84,15 +98,15 @@ namespace Ragnarok.NicoNico.Video.Tests
                 "音楽", "初音ミク", "公式デモ", "VOCALOID"
             };
             Assert.LessOrEqual(tags.Count(), video.TagList.Count());
-            tags.ForEachWithIndex((_, i) => Assert.AreEqual(_, video.TagList[i]));
+            tags.ForEach(_ => Assert.True(video.TagList.Contains(_)));
         }
 
-        private void TestSM500873(VideoData video, bool removeTag)
+        private void TestSM500873(VideoData video, RemoveTagType removeTagType)
         {
             Assert.AreEqual("sm500873", video.IdString);
             Assert.AreEqual(-1, video.ThreadId);
             Assert.AreEqual("組曲『ニコニコ動画』 ", video.Title);
-            Assert.AreEqual(RemoveTag(removeTag,
+            Assert.AreEqual(RemoveTag(removeTagType,
                 "<font size=\"+2\">700万再生、ありがとうございました。<br />" +
                 "記念動画公開中です ⇒ (<a href=\"http://www.nicovideo.jp/watch/sm14242201\" class=\"watch\">sm14242201</a>)<br />" +
                 "</font><br />" +
@@ -103,9 +117,9 @@ namespace Ragnarok.NicoNico.Video.Tests
                 "<a href=\"http://www.nicovideo.jp/user/145217\" target=\"_blank\">user/145217</a>"),
                 video.Description);
             Assert.AreEqual(DateTime.Parse("2007-06-23T18:27:06+09:00"), video.StartTime);
-            Assert.GreaterOrEqual(video.ViewCounter, 8882556);
-            Assert.GreaterOrEqual(video.CommentCounter, 4422089);
-            Assert.GreaterOrEqual(video.MylistCounter, 140981);
+            Assert.GreaterOrEqual(video.ViewCounter, 8882231);
+            Assert.GreaterOrEqual(video.CommentCounter, 4412089);
+            Assert.GreaterOrEqual(video.MylistCounter, 130981);
 
             var tags = new string[]
             {
@@ -113,17 +127,17 @@ namespace Ragnarok.NicoNico.Video.Tests
                 "ニコニコオールスター",
             };
             Assert.LessOrEqual(tags.Count(), video.TagList.Count());
-            tags.ForEachWithIndex((_, i) => Assert.AreEqual(_, video.TagList[i]));
+            tags.ForEach(_ => Assert.True(video.TagList.Contains(_)));
         }
 
-        private void Test1441099865(VideoData video, bool removeTag)
+        private void Test1441099865(VideoData video, RemoveTagType removeTagType)
         {
             Assert.IsNotNull(video);
 
             Assert.AreEqual("so27063885", video.IdString);
-            Assert.AreEqual(removeTag ? -1 : 1441099865, video.ThreadId);
+            Assert.AreEqual(removeTagType == RemoveTagType.None ? 1441099865 : -1, video.ThreadId);
             Assert.AreEqual("【9/30まで】将棋プレミアム【無料トライアルキャンペーン実施中】", video.Title);
-            Assert.AreEqual(RemoveTag(removeTag,
+            Assert.AreEqual(RemoveTag(removeTagType,
                 "囲碁・将棋チャンネルの新会員サービス【将棋プレミアム】が8月10日(月)よりスタート！<br>" +
                 "いつでもどこでも見られるオンデマンドサービスをはじめ、会員イベントやプレミアムグッズプレゼントなど将棋ファン必見のサービスです！<br><br>" +
                 "9月30日(水)まで無料メルマガ会員登録を行うと、すべてのコンテンツが見放題となる<br>" +
@@ -131,7 +145,7 @@ namespace Ragnarok.NicoNico.Video.Tests
                 "詳しくは将棋プレミアム(<a href=\"http://www.igoshogi.net/shogipremium/\" target=\"_blank\">http://www.igoshogi.net/shogipremium/</a>)へ今すぐアクセス☆"),
                 video.Description);
             Assert.AreEqual(DateTime.Parse("2015-09-01T18:30:00+09:00"), video.StartTime);
-            Assert.GreaterOrEqual(video.ViewCounter, 194);
+            Assert.GreaterOrEqual(video.ViewCounter, 190);
             Assert.GreaterOrEqual(video.CommentCounter, 0);
             Assert.GreaterOrEqual(video.MylistCounter, 0);
 
@@ -141,7 +155,7 @@ namespace Ragnarok.NicoNico.Video.Tests
                 "将棋プレミアム", "囲碁将棋チャンネル"
             };
             Assert.AreEqual(tags.Count(), video.TagList.Count());
-            tags.ForEachWithIndex((_, i) => Assert.AreEqual(_, video.TagList[i]));
+            tags.ForEach(_ => Assert.True(video.TagList.Contains(_)));
         }
 
         /// <summary>
@@ -150,11 +164,11 @@ namespace Ragnarok.NicoNico.Video.Tests
         [Test()]
         public void CreateFromApiTest()
         {
-            TestSM9(VideoData.CreateFromApi("sm9"), true);
-            TestSM941537(VideoData.CreateFromApi("sm941537"), true);
-            TestSM500873(VideoData.CreateFromApi("sm500873"), true);
+            TestSM9(VideoData.CreateFromApi("sm9"), RemoveTagType.All);
+            TestSM941537(VideoData.CreateFromApi("sm941537"), RemoveTagType.All);
+            TestSM500873(VideoData.CreateFromApi("sm500873"), RemoveTagType.All);
             Test1441099865(VideoData.CreateFromApi(
-                "http://www.nicovideo.jp/watch/1441099865?eco=1"), true);
+                "http://www.nicovideo.jp/watch/1441099865?eco=1"), RemoveTagType.All);
 
             Assert.Catch(() =>
                 VideoData.CreateFromApi("sm44422222222222222"));
@@ -168,17 +182,17 @@ namespace Ragnarok.NicoNico.Video.Tests
         [Test()]
         public void CreateFromPageTest()
         {
-            TestSM9(VideoData.CreateFromPage("sm9", this.cc), false);
+            TestSM9(VideoData.CreateFromPage("sm9", this.cc), RemoveTagType.None);
             Console.WriteLine("Waiting ...");
             Thread.Sleep(4000);
-            TestSM941537(VideoData.CreateFromPage("sm941537", this.cc), false);
+            TestSM941537(VideoData.CreateFromPage("sm941537", this.cc), RemoveTagType.None);
             Console.WriteLine("Waiting ...");
             Thread.Sleep(4000);
-            TestSM500873(VideoData.CreateFromPage("sm500873", this.cc), false);
+            TestSM500873(VideoData.CreateFromPage("sm500873", this.cc), RemoveTagType.None);
             Console.WriteLine("Waiting ...");
             Thread.Sleep(4000);
             Test1441099865(VideoData.CreateFromPage(
-                "http://www.nicovideo.jp/watch/1441099865?eco=1", this.cc), false);
+                "http://www.nicovideo.jp/watch/1441099865?eco=1", this.cc), RemoveTagType.None);
             Thread.Sleep(4000);
 
             Assert.Catch(() =>
@@ -195,12 +209,19 @@ namespace Ragnarok.NicoNico.Video.Tests
         public void SnapshotApiTest()
         {
             var vs = SnapshotApi.Search("レッツゴー！陰陽師（フルコーラスバージョン）");
-            TestSM9(vs.OrderByDescending(_ => _.ViewCounter).FirstOrDefault(), false);
+            TestSM9(vs.OrderByDescending(_ => _.ViewCounter).FirstOrDefault(), RemoveTagType.ATagOnly);
 
-            /*TestSM941537(VideoData.CreateFromPage("sm941537", this.cc), false);
-            TestSM500873(VideoData.CreateFromPage("sm500873", this.cc), false);
-            Test1441099865(VideoData.CreateFromPage(
-                "http://www.nicovideo.jp/watch/1441099865?eco=1", this.cc), false);*/
+            vs = SnapshotApi.Search("クリプトン開発のVOCALOID。つまり音声合成ソフトです");
+            TestSM941537(vs.OrderByDescending(_ => _.ViewCounter).FirstOrDefault(), RemoveTagType.ATagOnly);
+
+            vs = SnapshotApi.Search("ニコニコ動画(β・γ)で人気のあった曲などを繋いで");
+            TestSM500873(vs.OrderByDescending(_ => _.ViewCounter).FirstOrDefault(), RemoveTagType.ATagOnly);
+
+            vs = SnapshotApi.Search("CM 実験動画 囲碁将棋チャンネル 糸谷哲郎", false);
+            Test1441099865(vs.OrderByDescending(_ => _.ViewCounter).FirstOrDefault(), RemoveTagType.ATagOnly);
+
+            vs = SnapshotApi.Search("どｊどういｔｓｄｋｊぁ", false);
+            Assert.AreEqual(0, vs.Count());
         }
     }
 }
